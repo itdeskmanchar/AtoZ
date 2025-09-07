2ed7c31cfb6324257f36784a54f8c9dbd611abbf
tejaswini192005 <tejaswinidhage4@gmail.com>
2025-09-07 15:18:47 +0530

    new file:   A TO Z.png 	modified:   dealer.py

--- dealer.py
+++ dealer.py
@@ -1,1 +1,28 @@
-print("hello")
+
+import customtkinter
+import customtkinter as ctk
+from PIL import Image, ImageTk
+import qrcode
+
+app=customtkinter.CTk()
+app.title("Dealer Dashboard")
+app.geometry("1920x1080")
+
+
+label1=customtkinter.CTkLabel(app,text="Customer Name :",font=('arial',20,'bold'),fg_color='white')
+label1.place(x=50,y=80)
+
+label2=customtkinter.CTkLabel(app,text="Aadhar No.         :",font=('arial',20,'bold'),fg_color='white')
+label2.place(x=50,y=120)
+
+label3=customtkinter.CTkLabel(app,text="Bill                      :",font=('arial',20,'bold'),fg_color='white')
+label3.place(x=50,y=160)
+
+bill = 10
+
+upi = "rahulovhal25@ybl"
+
+
+
+
+app.mainloop()
